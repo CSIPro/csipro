f96e438b44f227383e214d7f01a182acde5379b0
saulprl <phasecross@hotmail.com>
2024-02-01 18:32:44 -0700

    Reboot project with pnpm

--- src/app/(payload)/admin/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react";
-
-export default function PayloadLayout({ children }: { children: ReactNode }) {
-  return (
-    <section className="min-h-screen w-full bg-muted text-white">
-      {children}
-    </section>
-  );
-}
