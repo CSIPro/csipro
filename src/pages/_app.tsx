a2018e2a57133c197a5aa107628e96759bc7972c
saulprl <phasecross@hotmail.com>
2024-02-09 17:29:40 -0700

    Add fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
 import "@/styles/globals.css";
+import { GeistSans } from "geist/font/sans";
 import type { AppProps } from "next/app";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+  variable: "--font-poppins",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={`${GeistSans.variable} ${poppins.variable}`}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
