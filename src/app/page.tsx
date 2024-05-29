59db62ea0ab5fc5d702f645ffad14a94957b8581
Kartleth <80503620+Kartleth@users.noreply.github.com>
2024-05-29 10:26:18 -0600

    Quienes somos (#14)
    * Add about us section in app
    
    * Update Event section in app
    
    * Update Event section in app with cms data
    
    * Fix lint errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 
 import EventCard from "@/components/event-card/event-card";
 import { SectionTitle } from "@/components/section-title/section-title";
+import { Button } from "@/components/ui/button";
 import {
   createResponseSchema,
   generateEmptyResponse,
@@ -30,12 +31,6 @@ const fetchEvents = async () => {
 export default async function Home() {
   const eventsRes = await fetchEvents();
 
-  const events = eventsRes.docs;
-
-  const event = events[0];
-
-  const eventDate = new Date(event.fecha);
-
   return (
     <>
       <div className="flex w-full flex-col items-center gap-4 sm:flex-row">
@@ -49,7 +44,6 @@ export default async function Home() {
         </div>
         <div className="max-w-full px-4 py-6 sm:max-w-2xl">
           <Image
-            layout="responsive"
             src="portada.jpg"
             width={500}
             height={300}
@@ -59,18 +53,64 @@ export default async function Home() {
           />
         </div>
       </div>
-      <SectionTitle>Nuevos eventos</SectionTitle>
-      <EventCard
-        title={event.titulo}
-        type={event.tipo}
-        date={eventDate}
-        duration={event.duracion}
-        image={`https://admin.csipro.isi.unison.mx${event.imagen_principal.url}`}
-        imageAlt={event.imagen_principal.alt}
-        spots={event.cupos}
-        location={event.lugar}
-        time={new Date(event.hora)}
-      />
+      <section>
+        <SectionTitle>¿QUIÉNES SOMOS?</SectionTitle>
+        <div className="flex w-full flex-col items-center gap-3 px-2">
+          <div className="relative w-fit">
+            <Image
+              src="nosotros.JPG"
+              width={500}
+              height={300}
+              alt="foto de portada"
+              className="rounded"
+              unoptimized
+            />
+            <div className="absolute -left-1.5 top-10 animate-floating rounded bg-[#0074F9] p-1.5 shadow-md shadow-black/25">
+              5 proyectos activos
+            </div>
+
+            <div className="absolute -right-1.5 top-[60%] animate-floating rounded bg-[#00C792] p-1.5 shadow-md shadow-black/25">
+              Fundado en 2014
+            </div>
+
+            <div className="absolute -left-1.5 bottom-12 animate-floating rounded bg-primary p-1.5 shadow-md shadow-black/25">
+              38 miembros
+            </div>
+          </div>
+          <p className="text-lg">
+            Somos estudiantes de la Universidad de Sonora en la búsqueda de
+            innovar. Realizamos e implementamos tecnologías, tales como software
+            desarrollado por nosotros
+          </p>
+
+          <Button variant="outline" className="text-base">
+            Más de nosotros
+          </Button>
+        </div>
+      </section>
+
+      <section className="w-full pb-3">
+        <SectionTitle>Nuevos eventos</SectionTitle>
+        <div className="flex flex-col items-center gap-3 px-2 sm:flex-row sm:justify-center">
+          {eventsRes.docs.slice(0, 3).map((event) => {
+            const eventDate = new Date(event.fecha);
+            return (
+              <EventCard
+                key={event.id}
+                title={event.titulo}
+                type={event.tipo}
+                date={eventDate}
+                duration={event.duracion}
+                image={`https://admin.csipro.isi.unison.mx${event.imagen_principal.url}`}
+                imageAlt={event.imagen_principal.alt}
+                spots={event.cupos}
+                location={event.lugar}
+                time={new Date(event.hora)}
+              />
+            );
+          })}
+        </div>
+      </section>
     </>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -71,10 +71,16 @@ const config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        floating: {
+          "0%": { transform: "translate(0,  0px);" },
+          "50%": { transform: "translate(0, 8px);" },
+          "100%": { transform: "translate(0, -0px);" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        floating: "floating 5s ease-in-out infinite",
       },
     },
   },
