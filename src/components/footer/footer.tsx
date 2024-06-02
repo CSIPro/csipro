5a6a06afcf72e4f921f40f51483c8fb549e067da
saulprl <phasecross@hotmail.com>
2024-06-02 14:09:32 -0700

    Adjust footer to match new desktop design

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import Link from "next/link";
+import Link, { LinkProps } from "next/link";
 import { FC, HTMLAttributes } from "react";
 
 import { cn } from "@/lib/utils";
@@ -26,57 +26,80 @@ export const Footer: FC<FooterProps> = (props) => {
         props.className,
       )}
     >
-      <div className="mx-auto flex max-w-5xl flex-col gap-4 p-4">
-        <div className="flex w-full flex-col justify-between gap-3 sm:flex-row sm:items-start">
-          <div className="flex w-full flex-col gap-2 font-light sm:w-auto sm:items-start sm:gap-1 sm:text-xs">
-            <Link href="#" className="p-1">
-              Inicio
-            </Link>
-            <Link href="#" className="p-1">
-              Eventos
-            </Link>
-            <Link href="#" className="p-1">
-              Nosotros
-            </Link>
-            <Link href="#" className="p-1">
-              Proyectos
-            </Link>
-            <Link href="#" className="p-1">
-              Contacto
-            </Link>
+      <div className="mx-auto flex max-w-5xl flex-col items-start gap-4 p-4">
+        <div className="flex items-center justify-center gap-4">
+          <div className="flex aspect-square w-10 items-center justify-center rounded-sm bg-primary p-1.5">
+            <Image
+              src="csipro.svg"
+              alt="Logo de CSI PRO"
+              width={36}
+              height={36}
+            />
           </div>
-          <div className="flex w-full flex-col items-center justify-center gap-8 border-y border-primary p-2 sm:w-auto sm:gap-4 sm:border-y-0 sm:border-l-2">
-            <div className="flex items-center justify-center gap-4">
-              <div className="flex aspect-square w-10 items-center justify-center rounded-sm bg-primary p-1.5 sm:w-8">
-                <Image
-                  src="csipro.svg"
-                  alt="Logo de CSI PRO"
-                  width={36}
-                  height={36}
-                />
-              </div>
-              <h1 className="text-2xl font-semibold sm:text-lg">CSI PRO</h1>
-            </div>
-            <div className="flex w-full items-center justify-around">
-              <Twitter />
-              <Facebook />
-              <Instagram />
-              <LinkedIn />
-              <GitHub />
-            </div>
+          <h1 className="text-2xl font-semibold">CSI PRO</h1>
+        </div>
+        <div className="flex w-full flex-col justify-between gap-4 md:flex-row md:items-center">
+          <ul className="flex w-full flex-col flex-wrap gap-2 sm:w-auto sm:flex-row sm:items-center sm:gap-4 md:gap-8 lg:gap-16">
+            <li>
+              <FooterLink href="#" className="p-1">
+                Inicio
+              </FooterLink>
+            </li>
+            <li>
+              <FooterLink href="#" className="p-1">
+                Eventos
+              </FooterLink>
+            </li>
+            <li>
+              <FooterLink href="#" className="p-1">
+                Nosotros
+              </FooterLink>
+            </li>
+            <li>
+              <FooterLink href="#" className="p-1">
+                Proyectos
+              </FooterLink>
+            </li>
+            <li>
+              <FooterLink href="#" className="p-1">
+                Contacto
+              </FooterLink>
+            </li>
+          </ul>
+          <div className="flex w-full items-center justify-around sm:w-auto sm:justify-normal sm:gap-6 lg:gap-8">
+            <Twitter />
+            <Facebook />
+            <Instagram />
+            <LinkedIn />
+            <GitHub />
           </div>
         </div>
-        <div className="py-[18px]"></div>
-        <div className="flex w-full flex-col items-center justify-center gap-1 text-center text-xs sm:flex-row sm:gap-2 sm:text-[10px]">
-          <p>© 2024 CSI PRO | Universidad de Sonora</p>
-          <p>Todos los derechos reservados.</p>
-          <p>
-            Departamento de Ingeniería Industrial y de Sistemas | Edificio
-            5J-205
-          </p>
+        <div className="sm:py-8"></div>
+        <hr className="w-full rounded-full border border-primary" />
+        <div className="sm:py-8"></div>
+        <div className="flex w-full flex-col items-center justify-center gap-1 text-center text-xs sm:flex-row sm:justify-between sm:gap-2 sm:text-start">
+          <div>
+            <p>&copy; 2024 CSI PRO | Universidad de Sonora</p>
+            <p>Todos los derechos reservados.</p>
+          </div>
+          <div className="sm:text-end">
+            <p>Departamento de Ingeniería Industrial y de Sistemas</p>
+            <p>Ubicados en el salón 5J-205</p>
+          </div>
         </div>
-        <div className="py-[18px]"></div>
       </div>
     </footer>
+  );
+};
+
+const FooterLink: FC<HTMLAttributes<HTMLAnchorElement> & LinkProps> = ({
+  className,
+  ...props
+}) => {
+  return (
+    <Link
+      className={cn("font-sans text-sm font-normal", className)}
+      {...props}
+    />
   );
 };
