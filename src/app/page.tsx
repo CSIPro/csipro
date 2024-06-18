ea37ad91f9fe7b943b59a906bb1666384c364cb6
Luis Hernandez <luisernestohernandez1@hotmail.com>
2024-06-18 14:50:34 -0700

    Fix hero desktop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -58,7 +58,7 @@ export default async function Home() {
 
   return (
     <>
-      <section className="relative flex w-full flex-col items-center gap-4 sm:flex-row">
+      <section className="relative flex w-full flex-col items-center justify-center gap-4">
         <GlowContainer>
           {/* Basic usage */}
           {/* <Glow breathe /> */}
@@ -68,24 +68,35 @@ export default async function Home() {
             <Glow className="bg-[radial-gradient(circle,rgba(115,115,115,1)_0%,rgba(49,0,163,1)_57%);] [clip-path:polygon(26%_10%,_49%_52%,_96%_56%,_93%_70%,_85%_80%,_77%_85%,_68%_90%,_58%_94%,_49%_94%,_38%_91%,_28%_87%,_18%_79%,_14%_71%,_8%_63%,_6%_55%,_7%_43%,_11%_31%,_15%_21%)]" />
           </GlowGroup>
         </GlowContainer>
-        <div className="px-4 py-6">
-          <h1 className="text-4xl font-semibold sm:text-5xl">
+        <div className="px-4 pb-6 pt-20">
+          <h1 className="text-center text-4xl font-bold  sm:text-6xl ">
             Un espacio de <span className="text-primary">desarrollo</span>,
-            <span className="text-primary"> innovación</span> e
-            <span className="text-primary"> investigación</span> en la
-            Universidad de Sonora
+            <br />
+            <span className="text-primary"> innovación</span> &
+            <span className="text-primary "> investigación</span>
+            <br />
+            <span className="text-xl font-semibold sm:text-4xl ">
+              en la Universidad de Sonora
+            </span>
           </h1>
         </div>
-        <div className="max-w-full px-4 py-6 sm:max-w-2xl">
+        <Button
+          variant="outline"
+          className="rounded-2xl px-5 py-7 text-2xl font-bold uppercase "
+        >
+          Get Started
+        </Button>
+        <div className="py-5"></div>
+        <div className=" relative aspect-video w-8/12   ">
           <Image
             src="portada.jpg"
-            width={500}
-            height={300}
+            fill={true}
             alt="foto de portada"
-            className="rounded"
+            className="rounded-2xl object-cover"
             unoptimized
           />
         </div>
+        <div className="py-24"></div>
       </section>
       <section>
         <SectionTitle>¿QUIÉNES SOMOS?</SectionTitle>
