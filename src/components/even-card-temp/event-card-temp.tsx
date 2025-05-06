70051027e9a418dbc0c78c9e08a4af6f89fe15b9
Kartleth <80503620+Kartleth@users.noreply.github.com>
2025-05-05 23:27:19 -0700

    Adjust layout for computers

--- src/components/even-card-temp/event-card-temp.tsx
+++ src/components/even-card-temp/event-card-temp.tsx
@@ -7,6 +7,7 @@ import { IoLocationSharp, IoStopwatchOutline } from "react-icons/io5";
 
 import { Button } from "@/components/ui/button";
 import { EventDate } from "@/models/events";
+
 import {
   BrandingHeader,
   BrandingHeaderTitle,
@@ -43,56 +44,60 @@ export const EventCardTemp: React.FC<EventCardTempProps> = ({
   const nextDate = parsedDates.find((d) => isFuture(d)) ?? parsedDates[0];
 
   return (
-    <div className="my-4 w-full max-w-sm rounded-xl border border-primary bg-[#160D2A] p-4 shadow-md">
-      <div className="flex select-none items-center justify-between pb-2">
-        <BrandingHeader>
-          <BrandingHeaderTitle>CSI PRO</BrandingHeaderTitle>
-          <BrandingHeaderHighlight>TALKS</BrandingHeaderHighlight>
-        </BrandingHeader>
+    <div className="p-2">
+      <div className="w-full max-w-sm rounded-xl border border-primary bg-[#160D2A] p-4 shadow-[0_0_12px_rgba(137,84,255,0.2)]">
+        <div className="flex select-none items-center justify-between pb-2">
+          <BrandingHeader>
+            <BrandingHeaderTitle>CSI PRO</BrandingHeaderTitle>
+            <BrandingHeaderHighlight>TALKS</BrandingHeaderHighlight>
+          </BrandingHeader>
 
-        <Chip variant="orange">
-          <ChipLabel uppercase>Multi-day</ChipLabel>
-        </Chip>
-      </div>
-
-      <hr className="border-1 border-[#2D1B55] pb-3" />
-
-      <div className="flex gap-4">
-        <div className="relative h-36 w-36 flex-shrink-0 overflow-hidden rounded">
-          <Image src={image} alt={imageAlt} fill className="object-cover" />
-          {isScheduled && (
-            <div className="absolute bottom-0 left-0 right-0 bg-primary px-2 text-center text-[10px] font-semibold text-white">
-              {spots === 1 ? "1 cupo disponilbe" : `${spots} cupos disponibles`}
-            </div>
-          )}
+          <Chip variant="orange">
+            <ChipLabel uppercase>Multi-day</ChipLabel>
+          </Chip>
         </div>
 
-        <div className="flex w-full flex-col justify-between text-white">
-          <div className="space-y-1">
-            <h2 className="text-base font-semibold">{title}</h2>
-            <div className="flex items-center gap-2 text-xs">
-              <IoLocationSharp />
-              <span>{location}</span>
-            </div>
+        <hr className="border-1 border-[#2D1B55] pb-3" />
 
-            <div className="flex items-center gap-2 text-xs">
-              <FaRegCalendar />
-              <span>{format(nextDate, "PPP", { locale: es })}</span>
-            </div>
-
-            <div className="flex items-center gap-2 text-xs">
-              <IoStopwatchOutline />
-              <span>{format(nextDate, "hh:mm aaaa", { locale: es })}</span>
-            </div>
+        <div className="flex gap-4">
+          <div className="relative h-36 w-36 flex-shrink-0 overflow-hidden rounded">
+            <Image src={image} alt={imageAlt} fill className="object-cover" />
+            {isScheduled && (
+              <div className="absolute bottom-0 left-0 right-0 bg-primary px-2 text-center text-[10px] font-semibold text-white">
+                {spots === 1
+                  ? "1 cupo disponilbe"
+                  : `${spots} cupos disponibles`}
+              </div>
+            )}
           </div>
 
-          <div className="flex justify-end pt-2">
-            <Button
-              variant="default"
-              className="rounded-s px-4 py-1 text-xs hover:bg-violet-700"
-            >
-              {isScheduled ? "Registrarse aquí" : "Más info"}
-            </Button>
+          <div className="flex w-full flex-col justify-between text-white">
+            <div className="space-y-1">
+              <h2 className="text-base font-semibold">{title}</h2>
+              <div className="flex items-center gap-2 text-xs">
+                <IoLocationSharp />
+                <span>{location}</span>
+              </div>
+
+              <div className="flex items-center gap-2 text-xs">
+                <FaRegCalendar />
+                <span>{format(nextDate, "PPP", { locale: es })}</span>
+              </div>
+
+              <div className="flex items-center gap-2 text-xs">
+                <IoStopwatchOutline />
+                <span>{format(nextDate, "hh:mm aaaa", { locale: es })}</span>
+              </div>
+            </div>
+
+            <div className="flex justify-end pt-2">
+              <Button
+                variant="default"
+                className="rounded-s px-4 py-1 text-xs hover:bg-violet-700"
+              >
+                {isScheduled ? "Registrarse aquí" : "Más info"}
+              </Button>
+            </div>
           </div>
         </div>
       </div>
--- src/components/search-bar.tsx/search-bar.tsx
+++ src/components/search-bar.tsx/search-bar.tsx
@@ -1,32 +1,51 @@
 "use client";
 
 import { Search } from "lucide-react";
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 
 interface SearchBarProps {
-  placeholder?: string;
+  shortPlaceholder?: string;
+  longPlaceholder?: string;
 }
 
 export const SearchBar: FC<SearchBarProps> = ({
-  placeholder = "Buscar en página...",
+  shortPlaceholder = "Buscar en página...",
+  longPlaceholder = "Buscar contenido en la página...",
 }) => {
+  const [placeholder, setPlaceholder] = useState(shortPlaceholder);
+  useEffect(() => {
+    const updatePlaceholder = () => {
+      if (window.innerWidth >= 1024) {
+        setPlaceholder(longPlaceholder);
+      } else {
+        setPlaceholder(shortPlaceholder);
+      }
+    };
+
+    updatePlaceholder();
+    window.addEventListener("resize", updatePlaceholder);
+    return () => window.removeEventListener("resize", updatePlaceholder);
+  }, [shortPlaceholder, longPlaceholder]);
+
   return (
-    <div className="rounded-2xl border border-[#281D3A] p-3 shadow-inner">
-      <div className="flex items-center overflow-hidden rounded-xl border border-white/20 bg-[#0f0f1a] text-white shadow-md">
-        <div className="flex items-center px-3">
-          <Search className="h-4 w-4 text-white/50" />
+    <div className="p-4">
+      <div className="rounded-2xl border border-[#281D3A] p-3 shadow-inner lg:p-4">
+        <div className="flex items-center overflow-hidden rounded-xl border border-white/20 bg-[#0f0f1a] text-white shadow-md">
+          <div className="flex items-center px-3">
+            <Search className="h-4 w-4 text-white/50" />
+          </div>
+          <input
+            type="text"
+            placeholder={placeholder}
+            className="w-full bg-transparent px-2 py-2 text-sm text-white placeholder-white/60 focus:outline-none"
+          />
+          <button
+            type="button"
+            className=" bg-primary px-4 py-2 text-sm font-semibold text-white transition-colors hover:bg-violet-700"
+          >
+            Buscar
+          </button>
         </div>
-        <input
-          type="text"
-          placeholder={placeholder}
-          className="w-full bg-transparent px-2 py-2 text-sm text-white placeholder-white/60 focus:outline-none"
-        />
-        <button
-          type="button"
-          className=" bg-primary px-4 py-2 text-sm font-semibold text-white transition-colors hover:bg-violet-700"
-        >
-          Buscar
-        </button>
       </div>
     </div>
   );
