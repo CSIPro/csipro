ad30d526b2a5d7077a6f9d3001bee89a35460858
Kartleth <80503620+Kartleth@users.noreply.github.com>
2025-05-05 18:21:55 -0700

    Add plaid pattern to Eventos page

--- src/app/eventos/page.tsx
+++ src/app/eventos/page.tsx
@@ -1,6 +1,7 @@
 import { EventCardTemp } from "@/components/even-card-temp/event-card-temp";
 import { GlowContainer, Glow } from "@/components/glow/glow";
 import { SearchBar } from "@/components/search-bar.tsx/search-bar";
+import { Section } from "@/components/section/section";
 import { SectionTitle } from "@/components/section-title/section-title";
 import {
   Facebook,
@@ -13,12 +14,51 @@ import {
 export default function Page() {
   return (
     <>
-      <section>
-        <GlowContainer className="relative h-[12rem]">
+      <Section className="relative -mt-[70px] overflow-hidden sm:pt-4">
+        <div className="absolute left-0 top-0 z-0 h-[50%] w-full overflow-hidden">
+          <svg
+            className="absolute inset-0 h-full w-full"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <defs>
+              <pattern
+                id="grid"
+                width="35"
+                height="35"
+                patternUnits="userSpaceOnUse"
+              >
+                <path
+                  d="M 35 0 L 0 0 0 35"
+                  fill="none"
+                  stroke="#3b2f58"
+                  strokeWidth="0.5"
+                />
+              </pattern>
+
+              <linearGradient id="fade" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="40%" stopColor="white" />
+                <stop offset="100%" stopColor="transparent" />
+              </linearGradient>
+
+              <mask id="fade-mask">
+                <rect width="100%" height="100%" fill="url(#fade)" />
+              </mask>
+            </defs>
+
+            <rect
+              width="100%"
+              height="100%"
+              fill="url(#grid)"
+              mask="url(#fade-mask)"
+            />
+          </svg>
+        </div>
+
+        <GlowContainer className="relative z-10 h-[8rem] sm:h-[12rem]">
           <Glow
             breathe
             size="normal"
-            className="left-1/2 top-0 -translate-x-1/2 -translate-y-1/3 bg-[radial-gradient(circle,rgba(95,54,190,0.62)_0%,rgba(95,54,190,0)_62%)]"
+            className="absolute left-1/2 top-0 -translate-x-1/2 -translate-y-1/4 bg-[radial-gradient(circle,rgba(95,54,190,0.62)_0%,rgba(95,54,190,0)_62%)]"
           />
         </GlowContainer>
 
@@ -30,7 +70,7 @@ export default function Page() {
           Inscribete para ampliar tus conocimientos
         </p>
 
-        <div className="p-4">
+        <div>
           <SearchBar placeholder="Buscar eventos..."></SearchBar>
         </div>
 
@@ -45,63 +85,65 @@ export default function Page() {
           <LinkedIn className="h-4 w-4" />
           <GitHub className="h-4 w-4" />
         </div>
-      </section>
+      </Section>
 
-      <SectionTitle>EVENTOS</SectionTitle>
+      <Section>
+        <SectionTitle>EVENTOS</SectionTitle>
 
-      <div className="px-2">
-        <EventCardTemp
-          title="GitHub talk"
-          image="/assets/github-event.jpg"
-          imageAlt="Taller de React"
-          dates={[
-            {
-              fecha_hora: "2025-06-10T10:00:00",
-              id: "",
-            },
-            {
-              fecha_hora: "2025-06-11T10:00:00",
-              id: "",
-            },
-          ]}
-          location="Auditorio Gustavo Figueroa"
-          spots={20}
-        />
-        <EventCardTemp
-          title="GitHub talk"
-          image="/assets/github-event.jpg"
-          imageAlt="Taller de React"
-          dates={[
-            {
-              fecha_hora: "2025-06-10T10:00:00",
-              id: "",
-            },
-            {
-              fecha_hora: "2025-06-11T10:00:00",
-              id: "",
-            },
-          ]}
-          location="Auditorio Gustavo Figueroa"
-          spots={20}
-        />
-        <EventCardTemp
-          title="GitHub talk"
-          image="/assets/github-event.jpg"
-          imageAlt="Taller de React"
-          dates={[
-            {
-              fecha_hora: "2025-06-10T10:00:00",
-              id: "",
-            },
-            {
-              fecha_hora: "2025-06-11T10:00:00",
-              id: "",
-            },
-          ]}
-          location="Auditorio Gustavo Figueroa"
-          spots={20}
-        />
-      </div>
+        <div className="px-2">
+          <EventCardTemp
+            title="GitHub talk"
+            image="/assets/github-event.jpg"
+            imageAlt="Taller de React"
+            dates={[
+              {
+                fecha_hora: "2025-06-10T10:00:00",
+                id: "",
+              },
+              {
+                fecha_hora: "2025-06-11T10:00:00",
+                id: "",
+              },
+            ]}
+            location="Auditorio Gustavo Figueroa"
+            spots={20}
+          />
+          <EventCardTemp
+            title="GitHub talk"
+            image="/assets/github-event.jpg"
+            imageAlt="Taller de React"
+            dates={[
+              {
+                fecha_hora: "2025-06-10T10:00:00",
+                id: "",
+              },
+              {
+                fecha_hora: "2025-06-11T10:00:00",
+                id: "",
+              },
+            ]}
+            location="Auditorio Gustavo Figueroa"
+            spots={20}
+          />
+          <EventCardTemp
+            title="GitHub talk"
+            image="/assets/github-event.jpg"
+            imageAlt="Taller de React"
+            dates={[
+              {
+                fecha_hora: "2025-06-10T10:00:00",
+                id: "",
+              },
+              {
+                fecha_hora: "2025-06-11T10:00:00",
+                id: "",
+              },
+            ]}
+            location="Auditorio Gustavo Figueroa"
+            spots={20}
+          />
+        </div>
+      </Section>
     </>
   );
 }
