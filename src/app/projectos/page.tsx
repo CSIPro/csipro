f67f14f9042912d37a21af533f6da94019462afa
Andres Antelo  Figueroa <119352695+AndresAntelo21@users.noreply.github.com>
2025-05-20 00:42:41 -0700

    feat: add project page component with introductory content

---
+++ src/app/projectos/page.tsx
@@ -0,0 +1,10 @@
+export default function page() {
+    return (
+        <>
+            <div className="flex flex-col">
+                <h1 className="text-5xl font-bold text-center">¡Descubre los proyectos del <span className="text-primary">CSI PRO</span>!</h1>
+                <p className="text-center text-base">En CSI PRO encontrarás una variedad de proyectos de los miembros del laboratorio que resuelven problemas reales usando nuevas tecnologías.</p>
+            </div>
+        </>
+    )
+}
