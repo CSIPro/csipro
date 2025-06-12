900c6b6c1e7ddb24997417332ce240c77f7f2fe7
Saúl Ramos Laborín <saul.ramos@necodex.com>
2025-06-12 12:52:29 -0700

    feature: fade edges for projects carousel on desktop

--- src/components/projects-section/projects-section.tsx
+++ src/components/projects-section/projects-section.tsx
@@ -43,25 +43,31 @@ export default async function ProjectsSection() {
 
   return (
     <Carousel>
-      <CarouselContent className="-ml-4">
-        {projectsRes.docs.map((project) => (
-          <CarouselItem
-            key={project.id}
-            className="basis-5/6 sm:basis-3/4 md:basis-[45%] md:pl-8 lg:basis-[30%]"
-          >
-            <ProjectCard
+      <div className="relative">
+        {/* Left fade */}
+        <div className="pointer-events-none absolute left-0 top-0 z-10 hidden h-full w-16 bg-gradient-to-r from-white/90 to-transparent dark:from-muted lg:block" />
+        {/* Right fade */}
+        <div className="pointer-events-none absolute right-0 top-0 z-10 hidden h-full w-16 bg-gradient-to-l from-white/90 to-transparent dark:from-muted lg:block" />
+        <CarouselContent className="-ml-4">
+          {projectsRes.docs.map((project) => (
+            <CarouselItem
               key={project.id}
-              name={project.nombre}
-              subtitle={project.subtitulo}
-              systemType={project.tipo_sistema}
-              stack={project.tecnologias ?? []}
-              thumbnail={project.imagen_principal}
-            />
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-      <CarouselPrevious className="left-0 top-[45%] hidden size-11 disabled:hidden sm:inline-flex" />
-      <CarouselNext className="right-0 top-[45%] hidden size-11 disabled:hidden sm:inline-flex" />
+              className="basis-5/6 sm:basis-3/4 md:basis-[45%] md:pl-8 lg:basis-[30%]"
+            >
+              <ProjectCard
+                key={project.id}
+                name={project.nombre}
+                subtitle={project.subtitulo}
+                systemType={project.tipo_sistema}
+                stack={project.tecnologias ?? []}
+                thumbnail={project.imagen_principal}
+              />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+      </div>
+      <CarouselPrevious className="left-2 top-[45%] z-10 hidden size-11 disabled:hidden sm:inline-flex" />
+      <CarouselNext className="right-2 top-[45%] z-10 hidden size-11 disabled:hidden sm:inline-flex" />
       <CarouselNavigation name="Projects" />
     </Carousel>
   );
